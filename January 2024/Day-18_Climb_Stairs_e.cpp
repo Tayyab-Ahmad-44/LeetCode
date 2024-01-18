92f15c6385d263006e194d33062a8ac53854d6f1
Tayyab-Ahmad-44 <tayyabuetm24@gmail.com>
2024-01-18 06:01:33 +0500

    LeetCode

---
+++ January 2024/Day-18_Climb_Stairs_e.cpp
@@ -0,0 +1,48 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int climbStairs(int n)
+    {
+        vector<int> arr(n + 1, 0);
+
+        arr[0] = 1;
+        arr[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+            arr[i] = arr[i - 1] + arr[i - 2];
+
+        return arr[n];
+    }
+};
+
+/*
+
+70. Climbing Stairs
+
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+*/
