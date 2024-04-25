fb36f70005fee0b01c1e7f773f47c828a981643c
Tayyab-Ahmad-44 <tayyabuetm24@gmail.com>
2024-04-25 23:31:28 +0500

    LeetCode

---
+++ April 2024/25_longest_ideal_subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestIdealString(self, s: str, k: int) -> int:
+        ans = 1
+        for i in range(len(s)):
+            myStr = s[i]
+            for j in range(i+1, len(s)):
+                if abs(ord(myStr[len(myStr) - 1]) - ord(s[j])) <= k:
+                    myStr = myStr + s[j]
+            
+            if ans < len(myStr):
+                ans = len(myStr)
+
+        return ans
+s
+s = Solution()
+
+print(s.longestIdealString("abcd", 2))
