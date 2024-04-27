e2998ef22ccfcd2a0cd50fa0e8caf10c3c039fe8
Tayyab-Ahmad-44 <tayyabuetm24@gmail.com>
2024-04-27 17:24:30 +0500

    LeetCode Fav So Far

--- April 2024/26_min_fall_path_sum_h.py
+++ April 2024/26_min_fall_path_sum_h.py
@@ -101,3 +101,25 @@ grid = [[2,1,3],[5,4,6],[8,7,9]]
 
 print(sol.minFallingPathSum(grid))
             
+            
+"""
+1289. Minimum Falling Path Sum II
+
+Given an n x n integer matrix grid, return the minimum sum of a falling path with non-zero shifts.
+
+A falling path with non-zero shifts is a choice of exactly one element from each row of grid such that no two elements chosen in adjacent rows are in the same column.
+
+Example 1:
+Input: grid = [[1,2,3],[4,5,6],[7,8,9]]
+Output: 13
+Explanation: 
+The possible falling paths are:
+[1,5,9], [1,5,7], [1,6,7], [1,6,8],
+[2,4,8], [2,4,9], [2,6,7], [2,6,8],
+[3,4,8], [3,4,9], [3,5,7], [3,5,9]
+The falling path with the smallest sum is [1,5,7], so the answer is 13.
+
+Example 2:
+Input: grid = [[7]]
+Output: 7
+"""
